9039956e431c87fe6ee88e3c5c83639dd281f141
Michael Sacks <michaelsacks@gmail.com>
2015-04-28 10:36:30 -0600

    Initial Commit

---
+++ Controls/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Controls
+//
+//  Created by Michael Sacks on 4/28/15.
+//  Copyright (c) 2015 Michael Sacks. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Controls/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Controls
+//
+//  Created by Michael Sacks on 4/28/15.
+//  Copyright (c) 2015 Michael Sacks. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
